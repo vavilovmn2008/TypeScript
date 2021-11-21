ba0eeda9fe344c8aacd6df0440a5ae20a7893829
vavilovmn <vavilovmn@yandex.ru>
2021-11-21 23:05:49 +0300

    ДЗ1

---
+++ src/user.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { renderBlock } from './lib.js'
+
+export function renderUserBlock (
+  favoriteItemsAmount: number,
+  userName: string,
+  userAvatar: string  
+): void {
+  const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'
+  const hasFavoriteItems = favoriteItemsAmount ? true : false
+
+  renderBlock(
+    'user-block',
+    `
+    <div class="header-container">
+      <img class="avatar" src="/img/avatar.png" alt="Wade Warren" />
+      <div class="info">
+          <p class="name">Wade Warren</p>
+          <p class="fav">
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  )
+}
